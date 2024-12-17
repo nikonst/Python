5324ab5586404503d6dd1ab2c17419574f90db07
nikonst <nikonst@otenet.gr>
2024-12-17 12:25:08 +0200

    Use json file to load data

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -3,24 +3,27 @@ from typing import List
 from model import User, Gender, Role
 from uuid import UUID, uuid4
 
+import json
+
+# Open the JSON file
+with open('data/data.json') as f:
+    data = json.load(f)
+
 app = FastAPI()
 
-db: List[User] = [
-    User( 
-        id = UUID("781c30e6-157d-4819-a1bb-f7baa0419ee9"),
-        firstName = "Joe",
-        lastName = "Doe",
-        gender = Gender.male,
-        roles = [Role.admin]
-        ),
-    User( 
-        id = UUID("b4623d6f-7441-4519-a50a-0951796b963e"),
-        firstName = "Mary",
-        lastName = "Lawson",
-        gender = Gender.female,
-        roles = [Role.user]
-        ),
-]
+db: List[User] = []
+
+for person in data:
+    db.append(
+        User( 
+         id = uuid4(),
+         firstName = person["firstName"],
+         lastName = person["lastName"],
+         gender = person["gender"], # Gender.male,
+         email = person["email"],
+         roles = person["roles"]
+         )
+    )
 
 @app.get("/")
 async def root():
