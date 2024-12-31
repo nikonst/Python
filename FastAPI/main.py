7270018bce978f49dc184bddfea980369fbc3c4c
nikonst <nikonst@otenet.gr>
2024-12-31 11:31:35 +0200

    Fix expire to exp

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -54,11 +54,12 @@ for u in users:
 # Function to create a JWT token
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
+    print(to_encode)
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"expire": expire})
+    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
